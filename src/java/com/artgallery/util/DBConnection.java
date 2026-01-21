4d45063e3ae39e41e4f0f956c307af214da27d2c
shreya <dshreya23cs@student.mes.ac.in>
2026-01-21 06:47:26 +0530

    Fix DB Connection and RegisterServlet for railway MYSQLa

--- src/java/com/artgallery/util/DBConnection.java
+++ src/java/com/artgallery/util/DBConnection.java
@@ -1,13 +1,9 @@
 package com.artgallery.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-
 public class DBConnection {
-
     static {
         try {
-            // Load MySQL JDBC Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("✅ MySQL Driver Loaded");
         } catch (ClassNotFoundException e) {
@@ -15,22 +11,19 @@ public class DBConnection {
             e.printStackTrace();
         }
     }
-
     public static Connection getConnection() throws Exception {
-        // Railway MySQL Public Network credentials (your exact values)
         String host = "shuttle.proxy.rlwy.net";
         String port = "39052";
         String db   = "railway";
         String user = "root";
         String pass = "kJECCuRzRdAtFCykSamdCjDODBLWQrfL";
-
-        // JDBC URL
         String url = "jdbc:mysql://" + host + ":" + port + "/" + db +
                      "?useSSL=false&serverTimezone=UTC";
 
         System.out.println("🔗 Connecting to: " + url);
-
-        // Return connection
         return DriverManager.getConnection(url, user, pass);
     }
 }
+
+
+
